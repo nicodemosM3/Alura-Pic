3b50ef82f8a9afbc0807965c160e3b6330682ed9
Marcelo Marques <marcelommarq@gmail.com>
2024-04-17 13:19:28 -0300

    Autom. criação massa de dados

--- cypress/e2e/Cadastro/data-testes.cy.js
+++ cypress/e2e/Cadastro/data-testes.cy.js
@@ -1,10 +1,10 @@
-describe('Página de cadastro', () => {
+describe('Página de cadastro', () => { //Automação para criar massa de dados para testes
 
     beforeEach(() => {
         cy.visit('http://localhost:4200')
     }) 
 
-        const usuarios = require('../fixtures/usuarios.json')
+        const usuarios = require('../../fixtures/usuarios.json')
         usuarios.forEach(usuario => {
             
             it('Deve preencher os campos do formulário corretamente para cadastrar um novo usuário ${usuario.userName}', () => {
